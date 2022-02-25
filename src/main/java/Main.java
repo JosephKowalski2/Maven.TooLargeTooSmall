954bae4b6cc871c676e83a6d40a25a9cf0f06683
Joseph Kowalski <jkjr25@gmail.com>
2022-02-24 20:27:14 -0500

    Added code for number guessing game

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,32 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
+        int guessNumber = 2;
+        int userNumber = 0;
+        int numberGuesses = 0;
+        while (guessNumber != userNumber) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a number between 1 and 10");
+            userNumber = scanner.nextInt();
+
+            if (userNumber > guessNumber) {
+                System.out.println(userNumber + " is too large.");
+            } else if (userNumber < guessNumber) {
+                System.out.println(userNumber + " is too small.");
+            } else if (userNumber == guessNumber) {
+                System.out.println("You guessed correctly!");
+            }
+            numberGuesses++;
+
+        }
+        System.out.println("You guessed" + numberGuesses "times.");
     }
+
 }
